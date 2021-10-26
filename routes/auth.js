6d19d8c0fb77d6342323e56840138547f0b6f4ec
Nemesis <mezie.cjo@gmail.com>
2021-10-26 11:33:11 +0100

    added jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User")
 const router = require("express").Router()
 const CryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
 // dotenv.config()
 
@@ -27,20 +28,32 @@ router.post("/login", async (req, res) => {
 
         const user = await User.findOne({ username: req.body.username });
 
-        !user && res.status(401).json("Wrong credentials")
+        if (!user) {
+
+            res.status(401).json("Wrong credentials")
+        }
+
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET);
 
         const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-        originalPassword !== req.body.password && res.status(401).json("Wrong credentials")
+        if (originalPassword !== req.body.password) {
+
+            res.status(401).json("Wrong credentials")
+        }
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_KEY, { expiresIn: "3d" })
 
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others)
+        res.status(200).json({ ...others, accessToken })
+    } catch (err) {
 
-    } catch (error) {
-        res.status(500).json(error)
+        // console.log(err)
     }
 
 })
